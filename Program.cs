a89e0990efee7d598bfa0e3e5beda74c144eb5db
Subshribe <54627071+Subshribe@users.noreply.github.com>
2019-09-26 09:53:30 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using ImageClassLibrary;
+using System;
+using System.Drawing;
+using System.IO;
 
-namespace ImageConverterforms
+
+namespace Image
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            FileHandler importImage = new FileHandler();
+            Bitmap image = null;
+            ImageConverter imageConverter = new ImageConverter();
+            string filePath = null;
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Select an image to convert");
+                    filePath = Console.ReadLine();
+                }
+                else
+                {
+                    filePath = args[0];
+                }
+                if (importImage.TryIfFilePathIsAnImage(filePath))
+                {
+                    image = new Bitmap(filePath);
+                }
+                else
+                {
+                    while(!importImage.TryIfFilePathIsAnImage(filePath))
+                    {
+                        Console.WriteLine("Select an image to convert!");
+                    }
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File dont exist, try again");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Wrong input");
+                Environment.Exit(0);
+            }
+
+            Bitmap negativeImage = imageConverter.ConvertToNegative(image);
+            var negativeFileName = importImage.AddSuffixToFile(filePath, "Negative");
+            negativeImage.Save(negativeFileName);
+            negativeImage.Dispose();
+
+            var blackWhiteImage = imageConverter.ConvertToBlackAndWhite(image);
+            var blackAndWhiteFileName = importImage.AddSuffixToFile(filePath, "BlackAndWhite");
+            blackWhiteImage.Save(blackAndWhiteFileName);
+            blackWhiteImage.Dispose();
+
+            var blurredImage = imageConverter.ConvertToBlurred(image);
+            var blurredFileName = importImage.AddSuffixToFile(filePath, "Blurred");
+            blurredImage.Save(blurredFileName);
+            blurredImage.Dispose();
+
+            image.Dispose();
+
         }
     }
 }
+
