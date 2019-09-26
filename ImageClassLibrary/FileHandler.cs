0e6d816707b27df5eab3b8dd845202223b9794ec
Subshribe <54627071+Subshribe@users.noreply.github.com>
2019-09-26 11:30:36 +0200

    Alla filer

---
+++ ImageClassLibrary/FileHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ImageClassLibrary
+{
+    public class FileHandler
+    {
+        public bool TryIfFilePathIsAnImage(string filePath)
+        {
+            filePath = Path.GetExtension(filePath);
+            switch (filePath)
+            {
+                case ".jpg":
+                    return true;
+                case ".jfif":
+                    return true;
+                case ".png":
+                    return true;
+                case ".jpeg":
+                    return true;
+                default:
+                    return false;
+            }
+        }
+        public string AddSuffixToFile(string filePath, string suffix)
+        {
+            string newFileName = Path.GetDirectoryName(filePath) + Path.DirectorySeparatorChar + Path.GetFileNameWithoutExtension(filePath) + "_" + suffix + Path.GetExtension(filePath);
+            return newFileName;
+        }
+
+    }
+
+}
